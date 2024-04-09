d6ded619adb1fb093d3ed06e0500125600b0e1c9
zuzzang0824 <tiny0824@naver.com>
2024-04-09 15:25:40 +0900

    addInputBox

--- src/app/components/Todoinput/index.tsx
+++ src/app/components/Todoinput/index.tsx
@@ -1,3 +1,24 @@
+import styled from 'styled-components';
+
+const Box = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 15px 25px;
+  width: 100% auto;
+  font-size: 1.1em;
+  border-bottom: 1px solid #eee;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  border: none;
+  outline: 0;
+`;
+
 export default function TodoInput() {
-  return <></>;
+  return (
+    <Box>
+      <Input placeholder="할 일을 입력해 주세요." />
+    </Box>
+  );
 }
--- src/types/todo.d.ts
+++ src/types/todo.d.ts
@@ -2,7 +2,7 @@ interface ITodoItemContent {
   content: string;
 }
 
-interface ITodoItem {
+interface ITodoItem extends ITodoItemContent {
   id: string;
   completed: boolean;
   editing: boolean;
